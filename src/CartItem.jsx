1073243ba9c6b1b99a24d693eac2a41f41b1fb46
hwolteche <h.wolteche@gmail.com>
2025-03-05 16:16:41 -0500

    calculate totals

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,20 +9,32 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+        let total = 0;
+        cart.forEach((item) => {
+            const quantity = item.quantity;
+            const cost = parseFloat(item.cost.substring(1));
+            total += cost * quantity;
+        })
+        return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity === 1){
+        dispatch(removeItem({ name: item.name, quantity: item.quantity}));
+    }
+    else{
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    }
   };
 
   const handleRemove = (item) => {
@@ -30,7 +42,14 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+        const quantity = item.quantity;
+        const cost = parseFloat(item.cost.substring(1));
+        return cost * quantity;
   };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+    };
 
   return (
     <div className="cart-container">
